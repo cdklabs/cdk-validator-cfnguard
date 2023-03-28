497dce8b869c91c98b6c3cfbc78f1e69340f0734
Eddie Peters <iameddie@amazon.com>
2023-03-28 19:58:01 +0000

    Add remaining tests for CT.S3.PR.1

--- test/rules/control-tower/s3/ct-s3-pr-1.test.ts
+++ test/rules/control-tower/s3/ct-s3-pr-1.test.ts
@@ -5,6 +5,8 @@ import {
   // Stage,
   Stack,
   aws_s3 as s3,
+  Token,
+  CfnParameter,
 } from 'aws-cdk-lib';
 import { CfnGuardValidator } from '../../../../src';
 
@@ -25,7 +27,7 @@ describe('CT.S3.PR.1', () => {
     });
 
     // WHEN
-    const stack = new Stack(app, 'Stack');
+    new Stack(app, 'Stack');
 
     // THEN
     expect(() => {
@@ -67,7 +69,7 @@ describe('CT.S3.PR.1', () => {
     ]));
   });
 
-  test.each([{ blockPublicAcls: true }])('Scenario 3 validation fails: %p', (configuration) => {
+  test.each([{ blockPublicAcls: true }, { blockPublicAcls: true, blockPublicPolicy: true, ignorePublicAcls: true }])('Scenario 3 validation fails: %p', (configuration) => {
     // GIVEN
     const app = new App({
       policyValidationBeta1: [
@@ -81,6 +83,143 @@ describe('CT.S3.PR.1', () => {
     // WHEN
     const stack = new Stack(app, 'Stack');
     new s3.CfnBucket(stack, 'Bucket', { publicAccessBlockConfiguration: configuration });
+
+    // THEN
+    expect(() => {
+      app.synth();
+    }).toThrow(/^Validation failed/i);
+
+    const report = JSON.parse(fs.readFileSync(path.join(app.outdir, 'policy-validation-report.json')).toString('utf-8').trim());
+    const rules = report.pluginReports.flatMap((r: any) => r.violations.flatMap((v: any) => v.ruleName));
+    expect(rules).toContain(
+      's3_bucket_level_public_access_prohibited_check',
+    );
+  });
+
+  test.each([{ blockPublicAcls: true, blockPublicPolicy: true, ignorePublicAcls: true, restrictPublicBuckets: false }])('Scenario 4 validation fails: %p', (configuration) => {
+    // GIVEN
+    const app = new App({
+      policyValidationBeta1: [
+        new CfnGuardValidator(),
+      ],
+      context: {
+        '@aws-cdk/core:validationReportJson': true,
+      },
+    });
+
+    // WHEN
+    const stack = new Stack(app, 'Stack');
+    new s3.CfnBucket(stack, 'Bucket', { publicAccessBlockConfiguration: configuration });
+
+    // THEN
+    expect(() => {
+      app.synth();
+    }).toThrow(/^Validation failed/i);
+
+    const report = JSON.parse(fs.readFileSync(path.join(app.outdir, 'policy-validation-report.json')).toString('utf-8').trim());
+    const rules = report.pluginReports.flatMap((r: any) => r.violations.flatMap((v: any) => v.ruleName));
+    expect(rules).toContain(
+      's3_bucket_level_public_access_prohibited_check',
+    );
+  });
+
+  test('Scenario 5 validation succeeds', () => {
+    // GIVEN
+    const app = new App({
+      policyValidationBeta1: [
+        new CfnGuardValidator(),
+      ],
+      context: {
+        '@aws-cdk/core:validationReportJson': true,
+      },
+    });
+
+    // WHEN
+    const stack = new Stack(app, 'Stack');
+    new s3.Bucket(stack,
+      'Bucket',
+      {
+        blockPublicAccess: {
+          blockPublicAcls: true,
+          blockPublicPolicy: true,
+          ignorePublicAcls: true,
+          restrictPublicBuckets: true,
+        },
+      });
+
+    // THEN
+    expect(() => {
+      app.synth();
+    }).not.toThrow();
+
+    const report = JSON.parse(fs.readFileSync(path.join(app.outdir, 'policy-validation-report.json')).toString('utf-8').trim());
+    const rules = report.pluginReports.flatMap((r: any) => r.violations.flatMap((v: any) => v.ruleName));
+    expect(rules).not.toContain([
+      's3_bucket_level_public_access_prohibited_check',
+    ]);
+  });
+
+  const nonBooleanConfigurations = [
+    { blockPublicAcls: Token.asAny(false), blockPublicPolicy: true, ignorePublicAcls: true, restrictPublicBuckets: true },
+  ];
+
+  test('Tokenized false value fails', () => {
+    // GIVEN
+    const app = new App({
+      policyValidationBeta1: [
+        new CfnGuardValidator(),
+      ],
+      context: {
+        '@aws-cdk/core:validationReportJson': true,
+      },
+    });
+
+    // WHEN
+    const stack = new Stack(app, 'Stack');
+    new s3.CfnBucket(stack, 'Bucket', {
+      publicAccessBlockConfiguration:
+       {
+         blockPublicAcls: Token.asAny(false),
+         blockPublicPolicy: true,
+         ignorePublicAcls: true,
+         restrictPublicBuckets: true,
+       },
+    });
+
+    // THEN
+    expect(() => {
+      app.synth();
+    }).toThrow(/^Validation failed/i);
+
+    const report = JSON.parse(fs.readFileSync(path.join(app.outdir, 'policy-validation-report.json')).toString('utf-8').trim());
+    const rules = report.pluginReports.flatMap((r: any) => r.violations.flatMap((v: any) => v.ruleName));
+    expect(rules).toContain(
+      's3_bucket_level_public_access_prohibited_check',
+    );
+  });
+
+  test('CFN stack parameter as property value fails', () => {
+    // GIVEN
+    const app = new App({
+      policyValidationBeta1: [
+        new CfnGuardValidator(),
+      ],
+      context: {
+        '@aws-cdk/core:validationReportJson': true,
+      },
+    });
+
+    // WHEN
+    const stack = new Stack(app, 'Stack');
+    const blockPublicAclsParameter = new CfnParameter(stack, 'BlockPublicAcls', { type: 'string', default: 'false' });
+    new s3.CfnBucket(stack, 'Bucket', {
+      publicAccessBlockConfiguration: {
+        blockPublicAcls: blockPublicAclsParameter.value,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true,
+      },
+    }),
 
     // THEN
     expect(() => {
